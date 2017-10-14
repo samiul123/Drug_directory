8bf1ce65f098f3ce6b14c7a7deabcffed537d39f
samiul123 <samiulmushfik123@gmail.com>
2017-10-15 00:59:13 +0600

    Initial commit

--- app/src/main/java/com/example/samiu/drug_directory/MainActivity.java
+++ app/src/main/java/com/example/samiu/drug_directory/MainActivity.java
@@ -7,13 +7,24 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import io.ghyeok.stickyswitch.widget.StickySwitch;
 
 public class MainActivity extends Activity implements View.OnClickListener{
 
-    Button vf_01_okayButton,yesButton,noButton,vf_03_okayButton,vf_04_okayButton,vf_05_doneButton;
+    Button vf_01_okayButton,yesButton,noButton,vf_03_okayButton,vf_04_okayButton,vf_05_doneButton,vf_06_donebutton;
     ViewFlipper vf;
     EditText vf_05_emailText,vf_06_passText;
+    TextView vf_06_givenEmail;
+    StickySwitch vf_07_stickySwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +47,16 @@ public class MainActivity extends Activity implements View.OnClickListener{
         vf_06_passText.setPadding(45,0,0,0);
         vf_05_doneButton = findViewById(R.id.vf_05_donebuttonId);
         vf_05_doneButton.setOnClickListener(this);
+        vf_06_givenEmail = findViewById(R.id.givenEmailId);
+        vf_06_donebutton = findViewById(R.id.vf_06_donebuttonId);
+        vf_06_donebutton.setOnClickListener(this);
+        vf_07_stickySwitch = findViewById(R.id.stick_switch);
+        vf_07_stickySwitch.setOnSelectedChangeListener(new StickySwitch.OnSelectedChangeListener() {
+            @Override
+            public void onSelectedChange(@NotNull StickySwitch.Direction direction,@NotNull String s){
+                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
@@ -57,7 +78,18 @@ public class MainActivity extends Activity implements View.OnClickListener{
             vf.showNext();
         }
         else if(view == vf_05_doneButton){
+            vf_06_givenEmail.setText(vf_05_emailText.getText());
             vf.showNext();
         }
+        else if(view == vf_06_donebutton){
+            vf.showNext();
+        }
+    }
+
+    public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
