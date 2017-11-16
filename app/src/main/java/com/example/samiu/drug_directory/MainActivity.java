937495b3c8819ee743289f5abd6adc7a89a3bf9d
samiul123 <samiulmushfik123@gmail.com>
2017-11-16 17:20:52 +0600

    integrated firebase with search suggestions

--- app/src/main/java/com/example/samiu/drug_directory/MainActivity.java
+++ app/src/main/java/com/example/samiu/drug_directory/MainActivity.java
@@ -1,95 +1,325 @@
 package com.example.samiu.drug_directory;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import android.widget.ViewFlipper;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.NetworkPolicy;
+import com.squareup.picasso.Picasso;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.Map;
 
-import io.ghyeok.stickyswitch.widget.StickySwitch;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
-public class MainActivity extends Activity implements View.OnClickListener{
 
-    Button vf_01_okayButton,yesButton,noButton,vf_03_okayButton,vf_04_okayButton,vf_05_doneButton,vf_06_donebutton;
-    ViewFlipper vf;
-    EditText vf_05_emailText,vf_06_passText;
-    TextView vf_06_givenEmail;
-    StickySwitch vf_07_stickySwitch;
+    private DatabaseReference mDatabase;
+    private DatabaseReference suggestionRef;
+    private DatabaseReference searchResultsRef;
+    private RecyclerView recyclerView;
+    private ProgressDialog mDialogue;
+    ArrayList<String> tradeNames = new ArrayList<>();
+    ArrayList<String> genericNames = new ArrayList<>();
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle toogle;
+    public NavigationView navView;
+    Toolbar toolbar;
+    ArrayList<String> names = new ArrayList<>();
+    MaterialSearchView materialSearchView;
+    FirebaseRecyclerAdapter<Drug, DrugViewHolder> fireBaseRecyclerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        vf_01_okayButton = findViewById(R.id.button);
-        vf_01_okayButton.setOnClickListener(this);
-        vf = findViewById(R.id.flipper01);
-        yesButton = findViewById(R.id.yesButtonId);
-        yesButton.setOnClickListener(this);
-        noButton = findViewById(R.id.noButtonId);
-        noButton.setOnClickListener(this);
-        vf_03_okayButton = findViewById(R.id.greetingsOkaybuttonId);
-        vf_03_okayButton.setOnClickListener(this);
-        vf_04_okayButton = findViewById(R.id.vf_04_OkaybuttonId);
-        vf_04_okayButton.setOnClickListener(this);
-        vf_05_emailText = findViewById(R.id.emailText);
-        vf_05_emailText.setPadding(45,0,0,0);
-        vf_06_passText = findViewById(R.id.passTextId);
-        vf_06_passText.setPadding(45,0,0,0);
-        vf_05_doneButton = findViewById(R.id.vf_05_donebuttonId);
-        vf_05_doneButton.setOnClickListener(this);
-        vf_06_givenEmail = findViewById(R.id.givenEmailId);
-        vf_06_donebutton = findViewById(R.id.vf_06_donebuttonId);
-        vf_06_donebutton.setOnClickListener(this);
-        vf_07_stickySwitch = findViewById(R.id.stick_switch);
-        vf_07_stickySwitch.setOnSelectedChangeListener(new StickySwitch.OnSelectedChangeListener() {
+
+
+        //recyclerView
+        recyclerView = findViewById(R.id.drug_list);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams) recyclerView.getLayoutParams();
+        marginLayoutParams.setMargins(0, 110, 0, 0);
+        recyclerView.setLayoutParams(marginLayoutParams);
+
+        //Configuring fireBase Database
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Drugs");
+        suggestionRef = FirebaseDatabase.getInstance().getReference().child("Drugs");
+        mDatabase.keepSynced(true);
+        suggestionRef.keepSynced(true);
+        suggestionRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onSelectedChange(@NotNull StickySwitch.Direction direction,@NotNull String s){
-                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_SHORT).show();
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                collectNames((Map<String, Object>)dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        searchResultsRef = FirebaseDatabase.getInstance().getReference().child("Drugs");
+
+        //progress
+        mDialogue = new ProgressDialog(this);
+
+        materialSearchView = findViewById(R.id.search_view_id);
+
+        navView = findViewById(R.id.nav_id);
+        navView.setNavigationItemSelectedListener(this);
+
+        toolbar = findViewById(R.id.toolbarID);
+        setSupportActionBar(toolbar);
+        searchViewCode();
+
+        drawerLayout = findViewById(R.id.mainDrawer);
+        toogle = new ActionBarDrawerToggle(this,drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toogle);
+        toogle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void collectNames(Map<String, Object> value) {
+        for(Map.Entry<String, Object> entry: value.entrySet()){
+            Map singleName = (Map) entry.getValue();
+            names.add((String) singleName.get("tradeName"));
+            tradeNames.add((String) singleName.get("tradeName"));
+            names.add((String) singleName.get("genericName"));
+            genericNames.add((String) singleName.get("genericName"));
+        }
+        String[] suggestions = names.toArray(new String[names.size()]);
+        materialSearchView.setSuggestions(suggestions);
+    }
+
+
+    private void searchViewCode() {
+        materialSearchView.setEllipsize(true);
+        materialSearchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(final String query) {
+
+                if(tradeNames.contains(query)){
+                    final String[] id = new String[1];
+
+                    suggestionRef.orderByChild("tradeName").equalTo(query).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            for(DataSnapshot child: dataSnapshot.getChildren()){
+                                id[0] = child.getKey();
+                                Toast.makeText(getApplicationContext(),query + " " + id[0], Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+                }
+
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        materialSearchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+
             }
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mDialogue.setMessage("Please wait...");
+        mDialogue.show();
+        recyclerViewShow(mDatabase);
+    }
+
+    public void recyclerViewShow(final DatabaseReference ref){
+        fireBaseRecyclerAdapter = new FirebaseRecyclerAdapter<Drug, DrugViewHolder>(
+                Drug.class,
+                R.layout.durg_list_row,
+                DrugViewHolder.class,
+                ref
+        ) {
+
+            @Override
+            protected void populateViewHolder(DrugViewHolder viewHolder, final Drug model, int position) {
+                viewHolder.setTradeName(model.getTradeName());
+                viewHolder.setGenericName(model.getGenericName());
+                viewHolder.setImage(getApplicationContext(), model.getImage());
+
+                mDialogue.dismiss();
+                viewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(getApplicationContext(), "ID: " + model.getId(), Toast.LENGTH_SHORT).show();
+                        Intent detailIntent = new Intent(MainActivity.this, DetailsActivity.class);
+                        detailIntent.putExtra("drug_id", model.getId());
+                        startActivity(detailIntent);
+                    }
+                });
+
+
+                Log.d("Finish", "dismiss");
+                ref.orderByChild("tradeName").equalTo(model.getTradeName()).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+//                        Drug drug = dataSnapshot.getValue(Drug.class);
+//                        System.out.println("DRUG:" + drug + " KEY:" + dataSnapshot.getKey() + " Children: " + dataSnapshot.getChildrenCount());
+                        for (DataSnapshot child : dataSnapshot.getChildren()) {
+                            System.out.println("Child key:" + child.getKey());
+                            model.setId(child.getKey());
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        };
+
+        recyclerView.setAdapter(fireBaseRecyclerAdapter);
+    }
 
     @Override
-    public void onClick(View view) {
-        if(view == vf_01_okayButton){
-            vf.showNext();
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(!toogle.onOptionsItemSelected(item)){
+            return true;
         }
-        else if(view == yesButton){
+        if(item.getItemId() == R.id.action_search){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.material_search_menu, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        materialSearchView.setMenuItem(item);
+        return true;
+    }
+
+
+    /**
+     * Called when an item in the navigation menu is selected.
+     *
+     * @param item The selected item
+     * @return true to display the item as the selected item
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.db_id ){
+            Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_SHORT).show();
+            drawerLayout.closeDrawers();
+            return true;
         }
-        else if(view == noButton){
-            vf.showNext();
+        return false;
+    }
+
+
+
+
+    public static class DrugViewHolder extends RecyclerView.ViewHolder{
+        View mView;
+        public DrugViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
         }
-        else if(view == vf_03_okayButton){
-            vf.showNext();
+
+        public void setTradeName(String tradeName){
+            TextView tradeTextView = mView.findViewById(R.id.trade_name_id);
+            tradeTextView.setText(tradeName);
         }
-        else if (view == vf_04_okayButton){
-            vf.showNext();
+        public void setGenericName(String genericName){
+            TextView genTextView = mView.findViewById(R.id.generic_name_id);
+            genTextView.setText(genericName);
         }
-        else if(view == vf_05_doneButton){
-            vf_06_givenEmail.setText(vf_05_emailText.getText());
-            vf.showNext();
-        }
-        else if(view == vf_06_donebutton){
-            vf.showNext();
+        public void setImage(Context context,String image){
+            ImageView imageView = mView.findViewById(R.id.drug_image_id);
+            final Context ctx = context;
+            final String img = image;
+            final ImageView imgView = imageView;
+            //Picasso.with(context).load(image).into(imageView);
+            Picasso.with(context).load(image).networkPolicy(NetworkPolicy.OFFLINE).into(imageView, new Callback() {
+                @Override
+                public void onSuccess() {
+
+                }
+
+                @Override
+                public void onError() {
+                    Picasso.with(ctx).load(img).into(imgView);
+                }
+            });
         }
     }
 
-    public static boolean isEmailValid(String email) {
-        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
-        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
+
+
+
+    @Override
+    public void onBackPressed() {
+        if(materialSearchView.isSearchOpen()){
+            materialSearchView.closeSearch();
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 }
